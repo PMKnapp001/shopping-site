d9a632c1fd77921f2256bbd061bf6b1e7132e221
PMKnapp001 <pmknapp001@outlook.com>
2022-11-20 15:38:18 -0500

    further study started

--- customers.py
+++ customers.py
@@ -4,4 +4,56 @@
 class Customer:
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(
+        self,
+        first_name,
+        last_name,
+        email,
+        password
+    ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+
+    def __repr__(self):
+
+        return (
+            f"<Customer: {self.first_name}, {self.last_name}, {self.email}>"
+        )
+
+
+def read_customers_from_file(filepath):
+
+    all_customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            (
+                first_name,
+                last_name,
+                email,
+                password
+            ) = line.strip().split("|")
+
+            all_customers[email] = Customer(
+                                        first_name,
+                                        last_name,
+                                        email,
+                                        password
+                                        )
+
+    return all_customers
+
+
+def get_by_email(email):
+    if customers.get(email) == None:
+        print("returning false")
+        return False
+    
+    else:
+        return customers[email]
+    
+customers = read_customers_from_file("customers.txt")
+
